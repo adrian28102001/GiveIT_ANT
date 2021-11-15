acde9c90e56dcc9b66ce2ff3f583e15679955581
Gherman Adrian <adrian.gherman@isa.utm.md>
2021-11-15 21:36:06 +0200

    Sider changes

--- src/components/Sider.js
+++ src/components/Sider.js
@@ -26,26 +26,11 @@ const SiderApp = () => {
                         Creaza un anunt
                     </Button></Link>
                     </div>
-                    <SubMenu key="sub1" icon={<UserOutlined/>} title="Mobila">
-                        <Menu.Item key="1">Scaun</Menu.Item>
-                        <Menu.Item key="2">Dulap</Menu.Item>
-                        <Menu.Item key="3">Masa</Menu.Item>
-                        <Menu.Item key="4">Pat</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" icon={<LaptopOutlined/>} title="Pentru copii">
-                        <Menu.Item key="5">Jucarii</Menu.Item>
-                        <Menu.Item key="6">Haine</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub3" icon={<NotificationOutlined/>} title="Tehnica">
-                        <Menu.Item key="7">Laptop</Menu.Item>
-                        <Menu.Item key="8">Telefon</Menu.Item>
-                        <Menu.Item key="9">Accesorii telefon</Menu.Item>
-                        <Menu.Item key="10">Accesorii laptop</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub4" icon={<LaptopOutlined/>} title="Sport">
-                        <Menu.Item key="11">Haltere</Menu.Item>
-                        <Menu.Item key="12">Accesorii sport</Menu.Item>
-                    </SubMenu>
+                        <Menu.Item key="1">Mobila</Menu.Item>
+                        <Menu.Item key="2">Pentru copii</Menu.Item>
+                        <Menu.Item key="3">Tehnica</Menu.Item>
+                        <Menu.Item key="4">Sport</Menu.Item>
+
                 </Menu>
             </Sider>
         </div>
