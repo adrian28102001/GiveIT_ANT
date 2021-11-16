2a6c62a9cc0d1667002e812f87ed828c1f096768
Gherman Adrian <adrian.gherman@isa.utm.md>
2021-11-16 12:10:09 +0200

    Working on login

--- springboot-backend/src/main/java/com/example/springboot/model/User.java
+++ springboot-backend/src/main/java/com/example/springboot/model/User.java
@@ -1,15 +1,18 @@
 package com.example.springboot.model;
 
-import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 //JPA annotations to map model to relational database table
 @Entity
 @Table(name = "users")
-public @Data class User {
+public
+class User implements UserDetails {
 
     @Id // Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,9 +37,109 @@ public @Data class User {
     @Column(name = "province")
     private String province;
 
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "authority", joinColumns = @JoinColumn(referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(referencedColumnName = "id"))
+    private List<Authority> authority = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "userid", referencedColumnName = "id")
     private List<PostModel> postModelList = new ArrayList<>();
-    //Getters Setters and constructors are created automatically by lombok library
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.enabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.enabled;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public List<PostModel> getPostModelList() {
+        return postModelList;
+    }
+
+    public void setPostModelList(List<PostModel> postModelList) {
+        this.postModelList = postModelList;
+    }
 }
---
+++ springboot-backend/src/main/java/com/example/springboot/services/CustomUserService.java
@@ -0,0 +1,23 @@
+package com.example.springboot.services;
+
+import com.example.springboot.model.User;
+import com.example.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUserEmail(email);
+
+        if(null==user){
+            //throw new UsernameNotFoundException()
+        }
+        return null;
+    }
+}
