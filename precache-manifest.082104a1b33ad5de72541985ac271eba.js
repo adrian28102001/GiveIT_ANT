4d3272955bdd99afe866db893505fab838c58437
Gherman Adrian <adrian.gherman@isa.utm.md>
2022-03-30 18:20:50 +0300

    Updates

---
+++ precache-manifest.082104a1b33ad5de72541985ac271eba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58661552a443088939af232cc204289",
+    "url": "/GiveIT_ANT/index.html"
+  },
+  {
+    "revision": "192a63749c8e290631f9",
+    "url": "/GiveIT_ANT/static/css/2.7a6f437a.chunk.css"
+  },
+  {
+    "revision": "b72fbeb8dacac8006b26",
+    "url": "/GiveIT_ANT/static/css/main.fe0df249.chunk.css"
+  },
+  {
+    "revision": "192a63749c8e290631f9",
+    "url": "/GiveIT_ANT/static/js/2.f0188792.chunk.js"
+  },
+  {
+    "revision": "9f05580a253b9ff08f1495f4627da369",
+    "url": "/GiveIT_ANT/static/js/2.f0188792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72fbeb8dacac8006b26",
+    "url": "/GiveIT_ANT/static/js/main.81dda063.chunk.js"
+  },
+  {
+    "revision": "204580c19e887063686c",
+    "url": "/GiveIT_ANT/static/js/runtime-main.60ce0ecc.js"
+  },
+  {
+    "revision": "7404a119ee85648281ac95d23eb78179",
+    "url": "/GiveIT_ANT/static/media/back3.7404a119.jpg"
+  },
+  {
+    "revision": "39e7dd4f0526d7bfc0ab1b6f1868ea6a",
+    "url": "/GiveIT_ANT/static/media/logo2.39e7dd4f.png"
+  }
+]);
